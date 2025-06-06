1ff6ef9965cdb595b5a7754f0febd84f45842495
daiki hayakawa <moko021917@outlook.jp>
2025-06-06 09:28:19 +0900

    ログインview作成 #3

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -5,56 +5,56 @@
 @endsection
 
 @section('content')
-<form class="register__form" action="/register" method="post">
-    @csrf
-    <h2 class="form__title">会員登録</h2>
-    <div class="form__group">
-        <span class="form__label">名前</span>
-        <div class="form__input">
-            <input type="text" name="name" value="{{ old('name') }}">
+    <form class="register__form" action="/register" method="post">
+        @csrf
+        <h2 class="form__title">会員登録</h2>
+        <div class="form__group">
+            <span class="form__label">名前</span>
+            <div class="form__input">
+                <input type="text" name="name" value="{{ old('name') }}">
+            </div>
+            <div class="form__error">
+                @error('name')
+                {{ $message }}
+                @enderror
+            </div>
         </div>
-        <div class="form__error">
-            @error('name')
-            {{ $message }}
-            @enderror
+        <div class="form__group">
+            <span class="form__label">メールアドレス</span>
+            <div class="form__input">
+                <input type="email" name="email" value="{{ old('email') }}">
+            </div>
+            <div class="form__error">
+                @error('email')
+                {{ $message }}
+                @enderror
+            </div>
         </div>
-    </div>
-    <div class="form__group">
-        <span class="form__label">メールアドレス</span>
-        <div class="form__input">
-            <input type="email" name="email" value="{{ old('email') }}">
+        <div class="form__group">
+            <span class="form__label">パスワード</span>
+            <div class="form__input">
+                <input type="password" name="password">
+            </div>
+            <div class="form__error">
+                @error('password')
+                {{ $message }}
+                @enderror
+            </div>
         </div>
-        <div class="form__error">
-            @error('email')
-            {{ $message }}
-            @enderror
+        <div class="form__group">
+            <span class="form__label">パスワード確認</span>
+            <div class="form__input">
+                <input type="password" name="password_confirmation">
+            </div>
+            <div class="form__error">
+                @error('password_confirmation')
+                {{ $message }}
+                @enderror
+            </div>
         </div>
-    </div>
-    <div class="form__group">
-        <span class="form__label">パスワード</span>
-        <div class="form__input">
-            <input type="password" name="password">
+        <div class="form__button">
+            <button class="form__button--submit" type="submit">登録する</button>
         </div>
-        <div class="form__error">
-            @error('password')
-            {{ $message }}
-            @enderror
-        </div>
-    </div>
-    <div class="form__group">
-        <span class="form__label">パスワード確認</span>
-        <div class="form__input">
-            <input type="password" name="password_confirmation">
-        </div>
-        <div class="form__error">
-            @error('password_confirmation')
-            {{ $message }}
-            @enderror
-        </div>
-    </div>
-    <div class="form__button">
-        <button class="form__button--submit" type="submit">登録する</button>
-    </div>
-    <a class="login__link" href="/login">ログインはこちら</a>
-</form>   
+        <a class="login__link" href="/login">ログインはこちら</a>
+    </form>   
 @endsection
